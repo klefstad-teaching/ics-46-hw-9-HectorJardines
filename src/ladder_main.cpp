a226381aee2db995a29568d159b65b5a82abac1b
HectorJardines <hjardine@uci.edu>
2025-03-15 05:11:07 +0000

    [Feature] include tests for ladder

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -1,7 +1,26 @@
 #include "ladder.h"
+#include <cassert>
+void verify_edit_dist()
+{
+    assert(edit_distance_within("wallow", "swallow", 1));
+    assert(!edit_distance_within("wallow", "shallow", 1));
+    assert(edit_distance_within("wallow", "wallow", 1));
+}
+
+void verify_is_adjacent()
+{
+    assert(is_adjacent("wallow", "wallow"));
+    assert(!is_adjacent("happy", "quintillion"));
+}
+
+void verify_ladder()
+{
+    verify_word_ladder();
+}
 
 int main()
 {
-    verify_word_ladder();
-    return 0;
+    verify_edit_dist();
+    verify_is_adjacent();
+    verify_ladder();
 }
